8777be1818feb094c2f5313e5b4f9767a77576c0
Susmitadash07 <91356059+Susmitadash@users.noreply.github.com>
2022-08-10 19:08:45 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+r = float(input("Enter radius of circle: "))
+a = 3.14159 * r * r
+print("The area of the circle with radius =", a)
